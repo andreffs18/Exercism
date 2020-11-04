f55843d4d1690b2e8e9ce1db2397b71585c75bd2
andreffs18 <andreffs18@gmail.com>
2020-11-04 23:59:22 +0000

    Add python raindrops exercice

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,11 @@
+def convert(number: int) -> str:
+    output = ""
+    if number % 3 == 0:
+        output += "Pling"
+    if number % 5 == 0:
+        output +=  "Plang"
+    if number % 7 == 0:
+        output += "Plong"
+    
+    return output if output else str(number)
+
