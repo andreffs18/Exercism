92d2f6ab33e99ab3710f71465a631f9f9e2fd1c4
andreffs18 <andreffs18@gmail.com>
2026-03-24 15:35:23 +0000

    Add python 'robot-name' exercise

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,25 +1,49 @@
 from random import sample
 from string import ascii_uppercase, digits
+from typing import Optional
+
 
 class Robot(object):
+    """A Robot with a randomly generated unique name.
 
-	name = None
+    Each robot has a name following the pattern of two uppercase letters
+    followed by three digits (e.g., 'AB123', 'XY789').
+    """
 
-	def __init__(self):
-		self.reset()
+    name: Optional[str] = None
 
-	def reset(self):
-		self.name = self._generate_new_name()
-	
-	def _generate_new_name(self):
-		letters = "".join(sample(ascii_uppercase, 2))
-		numbers = "".join(sample(digits, 3))
-		new_name = u'{}{}'.format(letters, numbers)
+    def __init__(self) -> None:
+        """
+        Initialize a new Robot with a randomly generated name.
+        """
+        self.reset()
 
-		if not self.name:			
-			return new_name
+    def reset(self) -> None:
+        """
+        Reset the robot to factory settings, generating a new random name.
 
-		if self.name == new_name:
-			return self._generate_new_name()
+        This method wipes the robot's name and assigns a new unique one.
+        """
+        self.name = self._generate_new_name()
 
-		return new_name
+    def _generate_new_name(self) -> str:
+        """
+        Generate a new unique robot name.
+
+        Creates a name with the format: 2 uppercase letters + 3 digits.
+        Ensures the new name is different from the current name.
+
+        Returns:
+            str: A new unique robot name (e.g., 'AB123').
+        """
+        letters = "".join(sample(ascii_uppercase, 2))
+        numbers = "".join(sample(digits, 3))
+        new_name = "{}{}".format(letters, numbers)
+
+        if not self.name:
+            return new_name
+
+        if self.name == new_name:
+            return self._generate_new_name()
+
+        return new_name
--- python/robot-name/robot_name_test.py
+++ python/robot-name/robot_name_test.py
@@ -5,12 +5,12 @@ from robot_name import Robot
 
 
 class RobotNameTest(unittest.TestCase):
-    # assertRegex() alias to adress DeprecationWarning
+    # assertRegex() alias to address DeprecationWarning
     # assertRegexpMatches got renamed in version 3.2
     if not hasattr(unittest.TestCase, "assertRegex"):
         assertRegex = unittest.TestCase.assertRegexpMatches
 
-    name_re = r'^[A-Z]{2}\d{3}$'
+    name_re = r"^[A-Z]{2}\d{3}$"
 
     def test_has_name(self):
         self.assertRegex(Robot().name, self.name_re)
@@ -21,21 +21,22 @@ class RobotNameTest(unittest.TestCase):
         self.assertEqual(robot.name, robot.name)
 
     def test_different_robots_have_different_names(self):
-        self.assertNotEqual(
-            Robot().name,
-            Robot().name
-        )
+        self.assertNotEqual(Robot().name, Robot().name)
 
     def test_reset_name(self):
         # Set a seed
         seed = "Totally random."
+
         # Initialize RNG using the seed
         random.seed(seed)
+
         # Call the generator
         robot = Robot()
         name = robot.name
+
         # Reinitialize RNG using seed
         random.seed(seed)
+
         # Call the generator again
         robot.reset()
         name2 = robot.name
@@ -43,5 +44,5 @@ class RobotNameTest(unittest.TestCase):
         self.assertRegex(name2, self.name_re)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
