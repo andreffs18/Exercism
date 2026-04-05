afdb2c0b22ca61edb57954bdf6e755020d82fbfe
andreffs18 <andreffs18@gmail.com>
2026-04-05 14:56:54 +0100

    Add python 'rna-transcription' exercise

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,9 +1,4 @@
 def to_rna(dna_strand):
-	# Correspondence between DNA and RNA strands
-	MAP = {
-		"G": "C",
-		"C": "G",
-		"T": "A",
-		"A": "U"
-	}
-	return "".join(map(lambda n: MAP.get(n), list(dna_strand)))
+    # Correspondence between DNA and RNA strands
+    MAP = {"G": "C", "C": "G", "T": "A", "A": "U"}
+    return "".join(map(lambda n: MAP.get(n), list(dna_strand)))
