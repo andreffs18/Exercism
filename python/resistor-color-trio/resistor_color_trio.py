e5b43db77a1f449e1f3961e3fac165148ab58e80
andreffs18 <andreffs18@gmail.com>
2026-04-16 22:52:29 +0100

    Add python 'resistor-color-trie' exercise

---
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -0,0 +1,30 @@
+BAND = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def label(colors):
+    ohms = (10 * BAND.index(colors[0]) + BAND.index(colors[1])) * (10 ** BAND.index(colors[2]))
+
+    if ohms > 1_000_000_000:
+        prefix = "giga"
+        ohms //= 1_000_000_000
+    elif ohms > 1_000_000:
+        prefix = "mega"
+        ohms //= 1_000_000
+    elif ohms > 1_000:
+        prefix = "kilo"
+        ohms //= 1_000
+    else:
+        prefix = ""
+
+    return f"{ohms} {prefix}ohms"
